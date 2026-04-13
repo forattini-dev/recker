419c488ced3d1edf0d5a10cd0b10237242cb99a5
Filipe Forattini <filipe@forattini.com.br>
2026-04-13 10:20:11 -0300

    refactor(protocols): re-export reddb from clients

--- src/protocols/index.ts
+++ src/protocols/index.ts
@@ -35,3 +35,32 @@ export {
   type TelnetResponse,
   type TelnetExecOptions
 } from './telnet.js';
+
+// RedDB
+export {
+  RedDbClient,
+  createRedDbClient,
+  reddb,
+  type RedDbTransportMode,
+  type RedDbResolvedTransport,
+  type RedDbQueryStats,
+  type RedDbQueryData,
+  type RedDbBatchQueryData,
+  type RedDbScanEntity,
+  type RedDbScanData,
+  type RedDbBulkInsertData,
+  type RedDbCapabilities,
+  type RedDbOperationMetrics,
+  type RedDbOperationEnvelope,
+  type RedDbQueryOptions,
+  type RedDbScanRequest,
+  type RedDbBulkInsertRowsRequest,
+  type RedDbBinaryValue,
+  type RedDbBulkInsertBinaryRequest,
+  type RedDbEnsureCollectionOptions,
+  type RedDbEnsureIndexOptions,
+  type RedDbWarmupIndexOptions,
+  type RedDbStatsOptions,
+  type RedDbWireTlsOptions,
+  type RedDbClientOptions
+} from '../clients/reddb.js';
